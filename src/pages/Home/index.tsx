eb67563bac30bb201356ba86ce1dac47525b318b
Pedro <82984609+Pedroaaaa@users.noreply.github.com>
2024-03-14 09:38:22 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <Wrapper>
       <Container>
+        <h1>Teste Git Actions</h1>
         <a href="/login">Login</a>
       </Container>
     </Wrapper>
